dc3341863ae4bf03604772ee2f7c2d69dd0a94ea
Vivek Bhushan <vivekbhushan604@gmail.com>
2026-08-19 01:53:37 +0530

    added side bars

---
+++ src/components/sidebarOne.tsx
@@ -0,0 +1,4 @@
+function sidebarOne() {
+  <div className="w-10 h-full bg-amber-400"></div>;
+}
+export default sidebarOne;
---
+++ src/components/sidebarTwo.tsx
@@ -0,0 +1,4 @@
+function sidebarTwo() {
+  <div className="w-full h-full bg-blue-700"></div>;
+}
+export default sidebarTwo;
